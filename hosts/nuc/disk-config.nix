b45122b55de91c07f88e5b5265102376168a71d1
CVan <cvandesande@opendmz.com>
2026-05-23 15:03:47 +0100

    add nuc disk id

--- hosts/nuc/disk-config.nix
+++ hosts/nuc/disk-config.nix
@@ -1,5 +1,5 @@
 # Replace this with the NUC's real whole-disk /dev/disk/by-id path before
 # running Disko. Use a whole disk path, not a -partN partition path.
 import ../../modules/disko/luks-btrfs.nix {
-  device = "/dev/disk/by-id/REPLACE_WITH_NUC_DISK";
+  device = "/dev/disk/by-id/wwn-0x5002538900035614";
 }
